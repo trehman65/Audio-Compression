35f2d20a795b3fbabba35eb6aead317370fb1b15
Talha Rehman <14beetrehman@seecs.edu.pk>
2017-06-01 08:41:52 +0500

    dev: Audio compression by picking dominant frames

--- audiocompression.m
+++ audiocompression.m
@@ -1,29 +1,67 @@
-%initialization
+%% Initialization
+
 close all
 clear all
 clc
 
-%load audio
+%% Flags
+%mode0=first N% DCT coefficients 
+%mode1=Dominant N% DCT coefficients 
+mode=1;
+
+
+%% load audio
 pathToAudio='/Users/talha/Desktop/DSPlabexam/sample.wav'
 [audio, samplingFrequency]=audioread(pathToAudio);
 
-%play audio
+%% Play audio
 %sound(audio, samplingFrequency)
 
-%pick N samples 
+%% Pick N samples 
 N=64;
 samplesToPick=1:N;
 thisWindow=0;
+reconstructedAudio=[];
 
-%selecting the frames
+%% Selecting the frames
 while samplesToPick(length(samplesToPick))<= length(audio)
     
     thisWindow=audio(samplesToPick);
     currentDCT=dct(thisWindow);
-    plot(currentDCT)
-    pause(1)
     
+    %% Pick dominant coefficients 
+    if mode==1
+        [sortedDCT,ind] = sort(abs(currentDCT),'descend');
+    
+        i = 1;
+        percentCoeff=.75;
+        
+        %iterating over the audio lenght to pick 
+        while norm(currentDCT(ind(1:i)))/norm(currentDCT) < percentCoeff
+           i = i + 1;
+        end
+        needed = i;
+        
+        currentDCT(ind(needed+1:end)) = 0;
+        reconstructedWindow = idct(currentDCT);
+        
+        reconstructedAudio=[reconstructedAudio;reconstructedWindow];
 
+        %% test the reconstructions
+%         plot(thisWindow);
+%         hold on 
+%         plot(reconstructedWindow);
+%         pause(1)
+%         close all
+
+    
+    end
+    
     %moving the window over the audio to pick next samples 
     samplesToPick=samplesToPick+N;
+
 end
+
+%% play the compressed audio
+
+sound(reconstructedAudio, samplingFrequency)
